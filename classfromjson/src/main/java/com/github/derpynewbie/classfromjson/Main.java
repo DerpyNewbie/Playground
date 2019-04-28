d54bfc1ee3f9be9cfca00cfaaf3f4c88cc8e036a
BuildTools <unconfigured@null.spigotmc.org>
2019-04-28 21:40:01 +0900

    refactor(Move javassist stuff to CtClassManager):

--- classfromjson/src/main/java/com/github/derpynewbie/classfromjson/Main.java
+++ classfromjson/src/main/java/com/github/derpynewbie/classfromjson/Main.java
@@ -1,10 +1,5 @@
 package com.github.derpynewbie.classfromjson;
 
-import javassist.ClassPool;
-import javassist.CtClass;
-import javassist.CtMethod;
-import javassist.CtNewMethod;
-
 public class Main {
 
 //    static String stringJson = "{\n" +
@@ -18,7 +13,7 @@ public class Main {
     public static void main(String[] args) {
         //TODO: somehow get argument from json
 
-        String className = "Test";
+        CtClassManager ctClassManager = new CtClassManager("Test");
         String methodSrc = "public void send() {" +
                 "   int a = 1;" +
                 "   int b = 2;" +
@@ -26,15 +21,9 @@ public class Main {
                 "   System.out.println(a + \" + \" + b + \" = \" + (a+b));" +
                 "}";
         try {
-            // Create class with javassist
-            ClassPool classPool = ClassPool.getDefault();
-            CtClass ctClass = classPool.makeClass(className);
-            CtMethod method = CtNewMethod.make(methodSrc, ctClass);
-
-            ctClass.addMethod(method);
-            // Call method using Reflection
-            Object object = ctClass.toClass().newInstance();
-            object.getClass().getMethod(method.getName()).invoke(object);
+            ctClassManager.createMethod(methodSrc);
+            //TODO: how do you know there is send method?
+            ctClassManager.doMethod("send");
         } catch (Exception ex) {
             ex.printStackTrace();
         }
