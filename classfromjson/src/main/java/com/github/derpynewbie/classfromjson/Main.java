012ba01424e05c3e85990739d67cd1b88c762b7c
BuildTools <unconfigured@null.spigotmc.org>
2019-04-30 02:45:53 +0900

    refactor(Change execute code due to change of Managers):

--- classfromjson/src/main/java/com/github/derpynewbie/classfromjson/Main.java
+++ classfromjson/src/main/java/com/github/derpynewbie/classfromjson/Main.java
@@ -1,29 +1,37 @@
 package com.github.derpynewbie.classfromjson;
 
-public class Main {
+import com.github.derpynewbie.classfromjson.tool.CtClassManager;
+import com.github.derpynewbie.classfromjson.tool.ObjectExecutor;
 
-//    static String stringJson = "{\n" +
-//            "        'users': {\n" +
-//            "            'methods': ('run','create', 'retrieve', 'update', 'destroy', 'list'),\n" +
-//            "            'run': \"public void run() {" +
-//            "System.out.println(\"Success!\");\"" +
-//            "       }\n" +
-//            "}";
+public class Main {
 
     public static void main(String[] args) {
         //TODO: somehow get argument from json
-
-        CtClassManager ctClassManager = new CtClassManager("Test");
+        String className = "Test";
         String methodSrc = "public void send() {" +
                 "   int a = 1;" +
                 "   int b = 2;" +
                 "   System.out.println(\"UwU Watz Dis!?\");" +
                 "   System.out.println(a + \" + \" + b + \" = \" + (a+b));" +
                 "}";
+        String paramMethodSrc = "public void add(int a, int b) {" +
+                "   int ans = a + b;" +
+                "   System.out.println(ans);" +
+                "}";
+        // Create javassist CtClass from CtClassManager
+        CtClassManager ctClassManager = new CtClassManager(className);
         try {
+            // Create methods
             ctClassManager.createMethod(methodSrc);
-            //TODO: how do you know there is send method?
-            ctClassManager.doMethod("send");
+            ctClassManager.createMethod(paramMethodSrc);
+            // Create class -> Create executor
+            ObjectExecutor objectExecutor = ctClassManager.toClass().createObjectExecutor(className);
+            // Call methods from executor
+            objectExecutor.callMethod("add", new Object[]{1, 2}, new Class<?>[]{int.class, int.class});
+            objectExecutor.callMethod("add", new Object[]{3, 4}, new Class<?>[]{int.class, int.class});
+            objectExecutor.callMethod("send");
+            // UwU
+            System.out.println("OwO");
         } catch (Exception ex) {
             ex.printStackTrace();
         }
